0fd91c8ac7c7af935b327f085a3ebb8f8b74cf18
Rajvi Sheth <rajvi.sheth@gmail.com>
2023-07-30 15:00:08 -0400

    Time: 81 ms (98.75%), Space: 23.7 MB (88.58%) - LeetHub

--- 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.py
+++ 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.py
@@ -6,19 +6,38 @@
 #         self.right = right
 class Solution:
     def maxPathSum(self, root: Optional[TreeNode]) -> int:
-        max_path = -float('inf')
-        def gain_from_subtree(node):
-            nonlocal max_path
-            if not node:
+        res = [root.val]
+        def dfs(root):
+            if not root:
                 return 0
             
-            gain_from_left = max(gain_from_subtree(node.left), 0)
-            gain_from_right = max(gain_from_subtree(node.right), 0)
+            leftMax = dfs(root.left)
+            rightMax = dfs(root.right)
             
-            max_path = max(max_path, gain_from_left + gain_from_right + node.val)
+            leftMax = max(leftMax, 0)
+            rightMax = max(rightMax, 0)
             
-            return max(gain_from_left + node.val, gain_from_right + node.val)
+            res[0] = max(res[0], root.val + leftMax + rightMax)
+            
+            return root.val + max(leftMax, rightMax)
+        
+        dfs(root)
+        return res[0]
+            
+#         max_path = -float('inf')
+#         def gain_from_subtree(node):
+#             nonlocal max_path
+#             if not node:
+#                 return 0
+            
+#             gain_from_left = max(gain_from_subtree(node.left), 0)
+#             gain_from_right = max(gain_from_subtree(node.right), 0)
+            
+#             max_path = max(max_path, gain_from_left + gain_from_right + node.val)
+            
+#             return max(gain_from_left + node.val, gain_from_right + node.val)
         
         
-        gain_from_subtree(root)
-        return max_path
+#         gain_from_subtree(root)
+#         return max_path
+    
