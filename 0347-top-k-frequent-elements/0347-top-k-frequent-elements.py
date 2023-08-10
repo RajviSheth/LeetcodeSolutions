4a1974c7a173ac901dc7a103cb4b811fb1bc0661
Rajvi Sheth <rajvi.sheth@gmail.com>
2023-08-10 19:22:30 -0400

    Time: 112 ms (67.08%), Space: 21.9 MB (21.61%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,12 +1,32 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        if k == len(nums):
+        if len(nums) == k:
             return nums
-        dict_count = Counter(nums)
-        sorted_dict = sorted(dict_count.items(), key=lambda item:item[1], reverse=True)
         
-        top_k_items = sorted_dict[:k]
+        count = {}
+        freq = [[] for i in range(len(nums)+1)]
+        for value in nums:
+            count[value] = 1 + count.get(value, 0)
+            
+        for n,c in count.items():
+            freq[c].append(n)
+            
+        res = []
+        for i in range(len(freq)-1, 0, -1):
+            for n in freq[i]:
+                res.append(n)
+                
+                if len(res) == k:
+                    return res
         
-        top_k_keys = [item[0] for item in top_k_items]
+#         if k == len(nums):
+#             return nums
+#         dict_count = Counter(nums)
+#         sorted_dict = sorted(dict_count.items(), key=lambda item:item[1], reverse=True)
+        
+#         top_k_items = sorted_dict[:k]
+        
+#         top_k_keys = [item[0] for item in top_k_items]
     
-        return top_k_keys
+#         return top_k_keys
+    
