5a5cac9b34ce01619ca44723b3e8d06a3f4d7d6b
Rajvi Sheth <rajvi.sheth@gmail.com>
2023-08-24 18:29:45 -0400

    Time: 42 ms (60.06%), Space: 16.4 MB (51.51%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -5,17 +5,12 @@ class Solution:
         for char in s:
             if char in dict_values:
                 if stack:
-                    top_element = stack.pop() 
+                    top_element = stack.pop()
                 else:
                     top_element = '#'
                 if dict_values[char] != top_element:
                     return False
             else:
                 stack.append(char)
-        return not stack   
-       
-        
-        
-        
-            
+        return not stack
         
