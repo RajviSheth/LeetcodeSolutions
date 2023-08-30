080b9c7f6f13bbe9b1841caef7dab4bdb2a98a05
Rajvi Sheth <rajvi.sheth@gmail.com>
2023-08-30 14:47:15 -0400

    Time: 58 ms (74.19%), Space: 20.3 MB (77.67%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.py
+++ 0141-linked-list-cycle/0141-linked-list-cycle.py
@@ -10,9 +10,18 @@ class Solution:
             return False
         slow = head
         fast = head.next
-        while slow != fast:
-            if fast == None or fast.next == None:
-                return False
+        # while slow != fast:
+        #     if fast == None or fast.next == None:
+        #         return False
+        #     slow = slow.next
+        #     fast = fast.next.next
+        # return True
+        
+        while fast and fast.next:
             slow = slow.next
             fast = fast.next.next
-        return True
+            
+            if slow == fast:
+                return True
+            
+        return False
