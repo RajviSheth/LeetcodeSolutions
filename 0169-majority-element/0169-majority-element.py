32521ab1d9cf86e181c401aa0e0396b8c6e4f177
Rajvi Sheth <rajvi.sheth@gmail.com>
2023-10-10 13:57:56 -0400

    Time: 156 ms (32.92%), Space: 17.7 MB (84.34%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -2,10 +2,7 @@ class Solution:
     def majorityElement(self, nums: List[int]) -> int:
         track = {}
         for num in nums:
-            if num in track:
-                track[num] += 1
-            else:
-                track[num] = 1
+            track[num] = track.get(num, 0) + 1
         for key, value in track.items():
             if value >= len(nums) / 2:
                 return key
