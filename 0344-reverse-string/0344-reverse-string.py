a38464b2658ae04721222d9a0d4e5911f7164df1
Rajvi Sheth <rajvi.sheth@gmail.com>
2024-02-02 11:11:16 -0500

    Time: 170 ms (68.30%), Space: 21 MB (66.41%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.py
+++ 0344-reverse-string/0344-reverse-string.py
@@ -6,9 +6,7 @@ class Solution:
         start = 0
         end = len(s) - 1
         while start < end:
-            # print(s[start], s[end])
             s[start], s[end] = s[end], s[start]
-            # print(s)
             start += 1
             end -= 1
                 
