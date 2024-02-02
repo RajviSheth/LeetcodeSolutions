b11e6616264fe81a2d9739477c5ebc61d1817935
Rajvi Sheth <rajvi.sheth@gmail.com>
2024-02-02 11:09:21 -0500

    Time: 179 ms, Memory: 20.7 MB - LeetHub

---
+++ reverse-string/reverse-string.py
@@ -0,0 +1,20 @@
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        start = 0
+        end = len(s) - 1
+        while start < end:
+            if s[start] and s[end]:
+                # print(s[start], s[end])
+                s[start], s[end] = s[end], s[start]
+                # print(s)
+                start += 1
+                end -= 1
+                
+                
+            
+            
+            
+        
