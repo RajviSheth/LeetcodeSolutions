d72079f06bd2b354b7ffef2e0097e88e2358effa
Rajvi Sheth <rajvi.sheth@gmail.com>
2024-02-02 22:41:35 -0500

    Time: 97 ms, Memory: 22.4 MB - LeetHub

--- group-anagrams/group-anagrams.py
+++ group-anagrams/group-anagrams.py
@@ -1,13 +1,22 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         track = collections.defaultdict(list)
-        for value in strs:
-            s_val = ''.join(sorted(value))
-            print(s_val)
-            if s_val in track:
-                track[s_val].append(value)
-            else:
-                track[s_val] = [value]
-                
+        for s in strs:
+            count = [0] * 26
+            for c in s:
+                count[ord(c)-ord('a')] += 1
+            track[tuple(count)].append(s)
+            
         return track.values()
+        
+        
+#         for value in strs:
+#             s_val = ''.join(sorted(value))
+#             print(s_val)
+#             if s_val in track:
+#                 track[s_val].append(value)
+#             else:
+#                 track[s_val] = [value]
+                
+#         return track.values()
                 
