6f14db25696760379ead2acd299b53ef7b6379e5
Rajvi Sheth <rajvi.sheth@gmail.com>
2024-02-10 12:14:00 -0500

    Time: 110 ms, Memory: 19.1 MB - LeetHub

--- single-number/single-number.py
+++ single-number/single-number.py
@@ -1,16 +1,20 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        hashmap = {}
-        check = set()
-        for value in nums:
-            if value not in hashmap:
-                hashmap[value] = 1
-            else:
-                hashmap[value] += 1
+        res = 0
+        for n in nums:
+            res = n ^ res
+        return res
+#         hashmap = {}
+#         check = set()
+#         for value in nums:
+#             if value not in hashmap:
+#                 hashmap[value] = 1
+#             else:
+#                 hashmap[value] += 1
                 
-        for key, value in hashmap.items():
-            if value == 1:
-                return key
+#         for key, value in hashmap.items():
+#             if value == 1:
+#                 return key
                 
                 
         
