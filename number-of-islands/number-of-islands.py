ad9493d2461b4a57bc671d2401365b8355059527
Rajvi Sheth <rajvi.sheth@gmail.com>
2024-02-11 19:30:00 -0500

    Time: 313 ms, Memory: 24.3 MB - LeetHub

--- number-of-islands/number-of-islands.py
+++ number-of-islands/number-of-islands.py
@@ -12,7 +12,7 @@ class Solution:
             visit.add((r, c))
             q.append((r, c))
             while q:
-                row, col = q.pop()
+                row, col = q.popleft()
                 directions = [(0, 1), (0, -1), (1, 0), (-1, 0) ]
                 for dr, dc in directions:
                     r, c = row + dr, col + dc
