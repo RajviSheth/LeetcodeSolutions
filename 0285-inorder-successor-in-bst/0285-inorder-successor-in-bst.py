b1da7265cc1a75bf3436a16ec0e23c96e2d5f3db
Rajvi Sheth <rajvi.sheth@gmail.com>
2024-02-14 16:21:43 -0500

    Time: 622 ms (5.05%), Space: 21.1 MB (37.69%) - LeetHub

--- 0285-inorder-successor-in-bst/0285-inorder-successor-in-bst.py
+++ 0285-inorder-successor-in-bst/0285-inorder-successor-in-bst.py
@@ -7,34 +7,43 @@
 
 class Solution:
     
-    previous, successor = None, None
-    
     def inorderSuccessor(self, root: TreeNode, p: TreeNode) -> Optional[TreeNode]:
         
-        # self.previous, self.inorder_successor_node = None, None
-        if p.right:
-            leftmost = p.right
-            while leftmost.left:
-                leftmost = leftmost.left
+        successor = None
+        while root:
+            print(successor)
+            if p.val >= root.val:
+                root = root.right
+                
+            else:
+                successor = root
+                root = root.left
+                
+        return successor
             
-            self.successor = leftmost
-        else:
-            self.inordercase2(root, p)
+#         if p.right:
+#             leftmost = p.right
+#             while leftmost.left:
+#                 leftmost = leftmost.left
             
-        return self.successor
+#             self.successor = leftmost
+#         else:
+#             self.inordercase2(root, p)
+            
+#         return self.successor
         
-    def inordercase2(self, node, p):
+#     def inordercase2(self, node, p):
         
-        if not node:
-            return
+#         if not node:
+#             return
         
-        self.inordercase2(node.left, p)
-        if p == self.previous and not self.successor:
-            self.successor = node
-            return
+#         self.inordercase2(node.left, p)
+#         if p == self.previous and not self.successor:
+#             self.successor = node
+#             return
         
-        self.previous = node
-        self.inordercase2(node.right, p)
+#         self.previous = node
+#         self.inordercase2(node.right, p)
         
         
                 
