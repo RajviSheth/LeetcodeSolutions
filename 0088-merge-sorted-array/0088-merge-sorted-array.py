08535b17a82c6d235fe8e04d2d8f49805231409b
Rajvi Sheth <rajvi.sheth@gmail.com>
2024-02-14 18:11:05 -0500

    Time: 31 ms (96.02%), Space: 16.4 MB (94.36%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -5,14 +5,18 @@ class Solution:
         """
         p1 = m-1
         p2 = n-1
-        w = m+n - 1
-        
-        for p in range(m+n-1, -1, -1):
-            if p2 < 0:
-                break
-            if p1 >= 0 and nums1[p1] > nums2[p2]:
-                nums1[p] = nums1[p1]
-                p1 -= 1
-            else:
-                nums1[p] = nums2[p2]
+        writer = m + n - 1
+        while p1 >=0 and p2>= 0:
+            if nums2[p2] >= nums1[p1]:
+                nums1[writer] = nums2[p2]
                 p2 -= 1
+            else:
+                nums1[writer] = nums1[p1]
+                p1 -=1
+            writer -= 1
+            
+        while p2 >= 0:
+            nums1[writer] = nums2[p2]
+            writer -= 1
+            p2-=1
+            
