5063dab2031cee944db2741c07e23fbc4ce1f518
Rajvi Sheth <rajvi.sheth@gmail.com>
2024-04-22 19:54:20 -0400

    Time: 43 ms, Memory: 17.7 MB - LeetHub

--- 0two-sum/0two-sum.py
+++ 0two-sum/0two-sum.py
@@ -1,10 +1,16 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         hashmap = {}
-        for idx, value in enumerate(nums):
-            diff = target - value
-            if diff in hashmap:
-                return [idx, hashmap[diff]]
-            else:
-                hashmap[value] = idx
         
+        for idx in range(len(nums)):
+            complement = target - nums[idx]
+            if complement in hashmap:
+                return [hashmap[complement], idx]
+            else:
+                hashmap[nums[idx]] = idx
+                
+                
+                
+            
+                    
+                
