ef2076f2ce700d665c834aca521dc8f5e0eb5942
Rajvi Sheth <rajvi.sheth@gmail.com>
2024-05-01 09:20:28 -0400

    Time: 43 ms, Memory: 17 MB - LeetHub

--- valid-palindrome/valid-palindrome.py
+++ valid-palindrome/valid-palindrome.py
@@ -3,14 +3,13 @@ class Solution:
         start = 0
         end = len(s) - 1
         while start < end:
-            while start< end and s[start].isalnum() and s[end].isalnum():
-                if s[start].lower() == s[end].lower():
-                    start += 1
-                    end -= 1
-                else:
-                    return False
             while start < end and not s[start].isalnum():
                 start += 1
             while start < end and not s[end].isalnum():
+                end -= 1
+            if s[start].lower() != s[end].lower():
+                return False
+            else:
+                start += 1
                 end -= 1
         return True
